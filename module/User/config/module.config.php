ba35bc2b93823ff00feeefe4fca5a3a06450a267
JuniorAmaralF <85956913+JuniorAmaralF@users.noreply.github.com>
2021-09-26 22:46:40 -0300

    Rotas

--- module/User/config/module.config.php
+++ module/User/config/module.config.php
@@ -1,7 +1,40 @@
 <?php
 //vai criar as rotas aqui
-return [
+namespace User;
 
+use Zend\Db\Sql\Predicate\Literal;
+use Zend\Router\Http\Segment;
+
+return [
+    'router' => [
+        'routes' => [
+            'user' => [
+                'type' => Literal::class,
+                'options' => [
+                    'route' => '/user',
+                    'default' => [
+                        'controller' => Controller\IndexController::class,
+                        'action'=> 'register'
+                    ]
+                ],
+                'may_terminate' => true,
+                'child_routes' => [
+                    'default' => [
+                        'type' => Segment::class,
+                        'options' => [
+                            //meusite.com/user/confirme/token/4085484 (md5)
+                            'route' => '[/:action][/token/:token]',
+                            'constraints' => [
+                                'action' => '[a-zA-z][a-zA-z0-9_-]*',
+                                'token' => '[[a-f0-9]{32}]$'
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+           
+        ]
+    ]
 ];
 
 ?>
