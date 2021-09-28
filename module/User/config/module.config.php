377007bb8379baf669cb54079d84c2510e2344fb
JuniorAmaralF <85956913+JuniorAmaralF@users.noreply.github.com>
2021-09-27 21:01:00 -0300

    controllers e views

--- module/User/config/module.config.php
+++ module/User/config/module.config.php
@@ -2,8 +2,9 @@
 //vai criar as rotas aqui
 namespace User;
 
-use Zend\Db\Sql\Predicate\Literal;
+use Zend\Router\Http\Literal;
 use Zend\Router\Http\Segment;
+use Zend\ServiceManager\Factory\InvokableFactory;
 
 return [
     'router' => [
@@ -12,7 +13,7 @@ return [
                 'type' => Literal::class,
                 'options' => [
                     'route' => '/user',
-                    'default' => [
+                    'defaults' => [
                         'controller' => Controller\IndexController::class,
                         'action'=> 'register'
                     ]
@@ -32,9 +33,25 @@ return [
                     ]
                 ]
             ],
-           
+        ]
+    ],
+    'controllers' => [
+        'factories' => [
+            Controller\IndexController::class => InvokableFactory::class
+        ]
+    ],
+    'view_manager' => [
+        'template_map' => [
+            'user/index/confirmed-email' => __DIR__.'/../view/user/index/confirmed-email.phtml',
+            'user/index/new-password' => __DIR__.'/../view/user/index/new-password.phtml',
+            'user/index/recovered-password' => __DIR__.'/../view/user/index/recovered-password.phtml',
+            'user/index/register' => __DIR__.'/../view/user/index/register.phtml',
+        ],
+        'template_path_stack' => [
+            __DIR__.'/../view',
         ]
     ]
+
 ];
 
 ?>
--- module/User/src/Controller/IndexController.php
+++ module/User/src/Controller/IndexController.php
@@ -1,18 +1,28 @@
 <?php
-/**
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
 
 namespace User\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
-
+//aqui fica as ações que podem ser executadas
 class IndexController extends AbstractActionController
 {
-    public function indexAction()
+    public function registerAction()
+    {
+        return new ViewModel();
+    }
+
+    public function recoveredPasswordAction()
+    {
+        return new ViewModel();
+    }
+
+    public function newPasswordAction()
+    {
+        return new ViewModel();
+    }
+
+    public function confirmedEmailAction()
     {
         return new ViewModel();
     }
