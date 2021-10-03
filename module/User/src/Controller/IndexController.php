62d6702644e81010e919239c94d35b62bef518d6
JuniorAmaralF <85956913+JuniorAmaralF@users.noreply.github.com>
2021-10-03 12:24:54 -0300

    modificando indexController

--- module/User/src/Controller/IndexController.php
+++ module/User/src/Controller/IndexController.php
@@ -2,15 +2,33 @@
 
 namespace User\Controller;
 
+use User\Form\UserForm;
+use User\Model\UserTable;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 //aqui fica as ações que podem ser executadas
 class IndexController extends AbstractActionController
 {
+    private $userForm;
+    private $userTable;
+
+    public function __construct(UserForm $userForm, UserTable $userTable)
+    {   
+        $this->userForm = $userForm;
+        $this->userTable = $userTable;
+    }
+
     public function registerAction()
     {
         $this->layout()->setTemplate('user/layout/layout');
-        return new ViewModel();
+
+        if($this->getRequest()->isPost()){
+            die('Post');
+        }
+
+        return new ViewModel([
+            'form' => $this->userForm->prepare()
+        ]);
     }
 
     public function recoveredPasswordAction()
