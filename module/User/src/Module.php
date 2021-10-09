c5c7274b10f55edad5992a019a5653c1c3b36fa3
JuniorAmaralF <85956913+JuniorAmaralF@users.noreply.github.com>
2021-10-09 11:48:47 -0300

    funcao onBootsrap

--- module/User/src/Module.php
+++ module/User/src/Module.php
@@ -2,13 +2,26 @@
 
 namespace User;
 
-class Module
+use User\Listener\SendRegisterListener;
+use Zend\EventManager\EventInterface;
+use Zend\ModuleManager\Feature\BootstrapListenerInterface;
+
+class Module implements BootstrapListenerInterface
 {
     public function getConfig()
     {
         return include __DIR__.'/../config/module.config.php';
     }
-}
 
+
+    public function onBootstrap(EventInterface $e)
+    {
+        $eventManager = $e->getApplication()->getEventManager();
+        $serviceManager = $e->getApplication()->getServiceManager();
+
+        (new SendRegisterListener($serviceManager))->attach($eventManager,100);
+    }
+
+}
 
 ?>
