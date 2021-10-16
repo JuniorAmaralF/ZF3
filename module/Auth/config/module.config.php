0e1f907217a609ac18a83da8c7bee602ff42696e
JuniorAmaralF <85956913+JuniorAmaralF@users.noreply.github.com>
2021-10-16 00:14:50 -0300

    rotas do auth login e logout

--- module/Auth/config/module.config.php
+++ module/Auth/config/module.config.php
@@ -1,7 +1,34 @@
 <?php
 
+namespace Auth;
+
+use Zend\Router\Http\Literal;
+
 return [
-    
+    'router' => [
+        'routes' => [
+            'auth.login' => [
+                'type' => Literal::class,
+                'options' =>[
+                    'route' => '/login',
+                    'default' => [
+                        'controller' => Controller\IndexController::class,
+                        'action' => 'login'
+                    ]
+                ]
+            ],
+            'auth.logout' => [
+                'type' => Literal::class,
+                'options' =>[
+                    'route' => '/logout',
+                    'default' => [
+                        'controller' => Controller\IndexController::class,
+                        'action' => 'logout'
+                    ]
+                ]
+            ],
+        ]
+    ]
 ];
 
 ?>
